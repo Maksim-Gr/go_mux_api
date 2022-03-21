dab1adfe263248b75b19cc6571ceaf080d62ef1a
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2022-03-21 17:12:08 +0300

    feat: scaffold minimum app && project structure

---
+++ app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+// Represents the main app
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {}
+
+func (a *App) Run(addr string) {}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Maksim-Gr/go_mux_api
+
+go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
