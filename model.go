74c8ac68fa3e7177f0ea2be27c844fa02acb30c9
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2022-04-11 09:24:15 +0300

    feat: createProduct query impl

--- model.go
+++ model.go
@@ -29,7 +29,13 @@ func (p *product) deleteProduct(db *sql.DB) error {
 }
 
 func (p *product) createProduct(db *sql.DB) error {
-	return errors.New("Not yet implemented")
+	err := db.QueryRow(
+		"INSERT INTO products(name,price) VALUES($1,$2) RETURNING id", p.Name, p.Price).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func getProducts(db *sql.DB, start, count int) ([]product, error) {
