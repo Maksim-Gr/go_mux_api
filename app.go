b467da9aafe3fd00e46c6f7cffdb7205f3bc050c
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2022-04-16 10:30:28 +0300

    feat: add product route

--- app.go
+++ app.go
@@ -35,8 +35,21 @@ func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
 	}
+
+	p := product{ID: id}
+	if err := p.getProduct(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Product not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	respondWithJson(w, http.StatusOK, p)
 }
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
