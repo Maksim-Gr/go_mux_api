a69622ccd9ec0ae3fcbacc7a8a28287c40fb7fa7
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2022-06-20 22:01:55 +0300

    feat: add prometheus metrics

--- internal/server/app.go
+++ internal/server/app.go
@@ -34,12 +34,13 @@ func (a *App) Initialize(user, password, dbname string) {
 		fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
 
 	var err error
-	a.DB, err = sql.Open("postges", connectionString)
+	a.DB, err = sql.Open("postgres", connectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	a.Router = mux.NewRouter()
+	a.Router.Use(prometheusMiddleware)
 
 	a.initializeRoutes()
 }
