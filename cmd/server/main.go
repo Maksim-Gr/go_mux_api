f9e5789847cd5d1e580dd50cfd7a1f44f02cca78
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2022-06-22 21:13:16 +0300

    feat: update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,7 +10,8 @@ func main() {
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
-
+		os.Getenv("APP_DB_NAME"),
+		os.Getenv("CACHE_HOST"),
+		os.Getenv("CACHE_PASS"))
 	a.Run(":8010")
 }
